7838664f18eb618552b238c6ad6e4c08c6570a9a
OpenShift guest <dimedrollum@gmail.com>
2013-03-05 12:07:45 -0500

    Re-structure

---
+++ Junk/public/DBtest.php
@@ -0,0 +1,19 @@
+<?php
+    $database = new mysqli('instance40644.db.xeround.com', 'dll', 'D4rkS!de', 'GitC9PHPXER', '3443');
+    
+    if($database->connect_errno>0){
+        echo 'Database error'. $database->connect_error;
+    }else{
+        echo 'Connected!';
+    }
+    $query = 'SELECT * FROM persons';
+    $result = $database->query($query);
+    
+    if (!$result){
+        echo 'Database query Error';
+    }else{
+        while($row = $result->fetch_assoc()){
+            echo 'Name: '. $row['Name'];
+        }
+    }
+    echo 'code completed';
