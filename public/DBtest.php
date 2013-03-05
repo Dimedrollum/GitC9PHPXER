802c67feee3a8d2f7d55ebbf549195fe1b6aa135
OpenShift guest <dimedrollum@gmail.com>
2013-03-05 10:06:32 -0500

    DB query test. Changed ERRNO

--- public/DBtest.php
+++ public/DBtest.php
@@ -1,7 +1,7 @@
 <?php
     $database = new mysqli('instance40644.db.xeround.com', 'dll', 'D4rkS!de', 'GitC9PHPXER', '3443');
     
-    if($database->connect_erno>0){
+    if($database->connect_errno>0){
         echo 'Database error'. $database->connect_error;
     }else{
         echo 'Connected!';
